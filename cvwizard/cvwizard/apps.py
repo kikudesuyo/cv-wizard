c9e6df663cd42574891b490a649d0b87934a27d4
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2023-11-04 23:16:15 +0900

    create .vscode

--- cvwizard/cvwizard/apps.py
+++ cvwizard/cvwizard/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CvwizardConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cvwizard'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cvwizard"
