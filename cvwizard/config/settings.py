805592578b0adc6b941deb78916c76b752071430
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2023-11-08 15:54:55 +0900

    make models

--- cvwizard/config/settings.py
+++ cvwizard/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "cvwizard.apps.CvwizardConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
