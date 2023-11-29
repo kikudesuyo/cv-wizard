2ac0029324f944f11b1cf04dd49783cc38ee51de
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2023-11-30 02:35:38 +0900

    fix: modify function base view to class base view in account/views.py

--- cvwizard/account/urls.py
+++ cvwizard/account/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("account/signup", views.SignUpView.sign_up, name="signup"),
-    path("account/delete", views.SignUpView.delete_account, name="delete"),
+    path("account/signup", views.SignUpView.as_view(), name="signup"),
+    path("account/delete", views.DeleteAccountView.as_view(), name="delete"),
 ]
--- cvwizard/account/views.py
+++ cvwizard/account/views.py
@@ -1,48 +1,52 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic.edit import FormView
+from django.views import View
 
 from .forms import DeleteAccountForm, SignUpForm
 from .models import AccountManagement
 
 
 # Create your views here.
-class SignUpView(FormView):
-    def sign_up(request):
-        """アカウントの新規登録"""
-        if request.method == "POST":
-            form = SignUpForm(request.POST)
-            if form.is_valid():
-                username = form.cleaned_data["username"]
-                password = form.cleaned_data["password"]
-                email = form.cleaned_data["email"]
-                if AccountManagement.objects.filter(email=email).exists():
-                    # アカウント登録ページに移動
-                    return HttpResponse("This Email already exists")
-                else:
-                    registration = AccountManagement(
-                        username=username, password=password, email=email
-                    )
-                    registration.add()
-                    # topページにリダイレクト
-                    return HttpResponse("Form is valid")
-        else:
-            form = SignUpForm()
+class SignUpView(View):
+    def get(self, request):
+        form = SignUpForm()
         return render(request, "account/signup.html", {"form": form})
 
-    def delete_account(request):
-        if request.method == "POST":
-            form = DeleteAccountForm(request.POST)
-            if form.is_valid():
-                password = form.cleaned_data["password"]
-                if AccountManagement.objects.filter(password=password).exists():
-                    deletion = AccountManagement.objects.filter(password=password)
-                    deletion.delete()
-                    # topページにリダイレクト
-                    return HttpResponse("Successfully deleted")
-                else:
-                    # アカウント削除ページに移動
-                    return HttpResponse("Password is not correct")
+    def post(self, request):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            email = form.cleaned_data["email"]
+            if AccountManagement.objects.filter(email=email).exists():
+                return HttpResponse("This Email already exists")
+            else:
+                registration = AccountManagement(
+                    username=username, password=password, email=email
+                )
+                registration.add()
+                # topページにリダイレクト
+                return HttpResponse("Form is valid")
         else:
-            form = DeleteAccountForm()
+            return HttpResponse("Form is invalid")
+
+
+class DeleteAccountView(View):
+    def get(self, request):
+        form = DeleteAccountForm()
         return render(request, "account/delete.html", {"form": form})
+
+    def post(self, request):
+        form = DeleteAccountForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data["password"]
+            if AccountManagement.objects.filter(password=password).exists():
+                deletion = AccountManagement.objects.filter(password=password)
+                deletion.delete()
+                # topページにリダイレクト
+                return HttpResponse("Successfully deleted")
+            else:
+                # アカウント削除ページに移動
+                return HttpResponse("Password is not correct")
+        else:
+            return HttpResponse("Form is invalid")
